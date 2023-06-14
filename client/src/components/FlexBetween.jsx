69aa9a75e6b3daf456440ba9ba8c34dadeb36b02
Calvin Costa <costacalvin0@gmail.com>
2023-06-13 21:14:13 -0400

    navbar created + light/dark mode toggle

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+const { default: styled } = require("@emotion/styled");
+
+const FlexBetween = styled(Box)({
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
+});
+
+export default FlexBetween;
