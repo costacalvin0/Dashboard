7c8af54ef79ae61a7b645e5e7263ba35168af7df
Calvin Costa <costacalvin0@gmail.com>
2023-06-19 20:14:41 -0400

    fixed mongoDB models + overallstats api

--- server/index.js
+++ server/index.js
@@ -15,11 +15,13 @@ import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
+  dataOverallStat,
 } from "./data/index.js";
 
 /* CONFIGURATION */
@@ -57,5 +59,6 @@ mongoose
     //Product.insertMany(dataProduct);
     //ProductStat.insertMany(dataProductStat);
     //Transaction.insertMany(dataTransaction);
+    //OverallStat.insertMany(dataOverallStat);
   })
   .catch((error) => console.log(`${error} did not connect`));
