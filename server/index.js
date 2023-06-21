f3f5418ff1a207b9d99ab171a1a087f9d44a5ae2
Calvin Costa <costacalvin0@gmail.com>
2023-06-20 23:15:21 -0400

    created Performance page + API

--- server/index.js
+++ server/index.js
@@ -16,12 +16,14 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
   dataOverallStat,
+  dataAffiliateStat,
 } from "./data/index.js";
 
 /* CONFIGURATION */
@@ -60,5 +62,6 @@ mongoose
     //ProductStat.insertMany(dataProductStat);
     //Transaction.insertMany(dataTransaction);
     //OverallStat.insertMany(dataOverallStat);
+    //AffiliateStat.insertMany(dataAffiliateStat);
   })
   .catch((error) => console.log(`${error} did not connect`));
